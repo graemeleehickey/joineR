8783c11ace6deb4d66d129d3cb4bb3a43528b2cf
Graeme Hickey <graemeleehickey@gmail.com>
2017-01-13 23:40:01 +0000

    Fix testhat bug + new test

--- tests/testthat/test-ancillary.R
+++ tests/testthat/test-ancillary.R
@@ -3,14 +3,15 @@ context("Ancillary functions work")
 
 test_that("simjoint works", {
   # simulate data
+  set.seed(1)
   d <- simjoint(10, sepassoc = TRUE)
   # tests
   expect_output(str(d), "List of 2")
-  expect_equal(dim(d$longitudinal), c(27, 7))
+  expect_equal(dim(d$longitudinal)[2], 7)
   expect_equal(dim(d$survival), c(10, 5))
 })
 
-test_that("simjoint works", {
+test_that("variogram works", {
   # load data + fit model
   data(mental)
   mental.unbalanced <- to.unbalanced(
@@ -28,3 +29,32 @@ test_that("simjoint works", {
   expect_equal(dim(vgm$svar), c(75, 2))
   expect_equal(vgm$sigma2, 104, tol = 1e-03)
 })
+
+test_that("to.unbalanced works", {
+  # convert
+  simul0 <- data.frame(
+    num = 1:10,
+    Y1.1 = rnorm(10), Y1.2 = rnorm(10),
+    Y2.1 = rnorm(10), Y2.2 = rnorm(10),
+    age = rnorm(10))
+  simul1 <- to.unbalanced(
+    simul0, 
+    id.col = 1, 
+    times = c(1, 2), 
+    Y.col = 2:5, 
+    other.col = 6)
+  simul2 <- to.balanced(
+    simul1, 
+    id.col = "num", 
+    time.col = "time",
+    Y.col = c("Y1.1", "Y2.1"), 
+    other.col = "age")
+  # tests
+  expect_equal(simul0, simul2, check.attributes = FALSE)
+  expect_equal(dim(simul0), c(10, 6))
+  expect_equal(dim(simul1), c(20, 5))
+  expect_output(str(simul1), "data.frame")
+  expect_output(str(simul2), "data.frame")
+})
+
+
