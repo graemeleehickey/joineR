cad019599fa733823fc917c6aa6de98e0ee855fb
Graeme Hickey <graemeleehickey@gmail.com>
2017-02-28 17:23:36 +0000

    Unit test added to close #33

--- tests/testthat/test-models.R
+++ tests/testthat/test-models.R
@@ -40,6 +40,32 @@ test_that("random-intercept and random-slope models", {
 })
 
 
+test_that("no survival model covariates", {
+  # load data + fit model
+  data(heart.valve)
+  heart.surv <- UniqueVariables(heart.valve, 
+                                var.col = c("fuyrs", "status"),
+                                id.col = "num")
+  heart.long <- heart.valve[, c("num", "time", "log.lvmi")]
+  heart.cov <- UniqueVariables(heart.valve, 
+                               c("age", "hs", "sex"), 
+                               id.col = "num")
+  heart.valve.jd <- jointdata(longitudinal = heart.long, 
+                              baseline = heart.cov, 
+                              survival = heart.surv, 
+                              id.col = "num", 
+                              time.col = "time")
+  fit <- joint(data = heart.valve.jd, 
+               long.formula = log.lvmi ~ 1 + time + hs, 
+               surv.formula = Surv(fuyrs, status) ~ 1, 
+               model = "intslope",
+               tol = 1e-05)
+  # tests
+  expect_is(fit, "joint")
+  expect_null(fit$coefficients$fixed$survival)
+})
+
+
 test_that("random-intercept only models", {
   # load data + fit model
   data(heart.valve)
